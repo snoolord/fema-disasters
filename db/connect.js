77d63989a7485f3c11cc9203c406019fc51d6e9d
Winston Zhao <winston.j.zhao@gmail.com>
2018-01-15 21:42:25 -0800

    fema-disasters/master - Added URI from heroku to connect to mongolab 	modified:   db/connect.js

--- db/connect.js
+++ db/connect.js
@@ -4,14 +4,18 @@ import config from '../core/config/config.dev';
 
 Mongoose.Promise = global.Promise;
 
-const connectToDb = async () => {
+const connectToDb = async() => {
   const dbHost = config.dbHost;
   const dbPort = config.dbPort;
   const dbName = config.dbName;
   try {
-    await Mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, {
-      useMongoClient: true,
-    });
+    if (process.env.NODE_ENV === 'production') {
+      Mongoose.connect('mongodb://heroku_nt34trk8:gnrl8933m1ia5nk2n0chlqe790@ds157057.mlab.com:57057/heroku_nt34trk8');
+    } else {
+      await Mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, {
+        useMongoClient: true,
+      });
+    }
     logger.info('Connected to mongo!!!');
   } catch (err) {
     logger.error('Could not connect to MongoDB');
